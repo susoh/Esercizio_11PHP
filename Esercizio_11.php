604441205421f893d2bbabb8fca0a1a5cfa1e760
Davide Susini <susini.davide@itismeucci.com>
2024-10-03 09:29:03 +0200

    esercizio finito

--- Esercizio_11.php
+++ Esercizio_11.php
@@ -6,6 +6,13 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <?php 
+        $linkBase = "https://xkcd.com";
+        $numLink = rand(2, 5);
+
+        for ($i = 1; $i <= $numLink; $i++) {
+            echo "<a href='$linkBase/$i/' target='_blank'>Pagina $i</a><br>";
+        }
+    ?>
 </body>
 </html>
